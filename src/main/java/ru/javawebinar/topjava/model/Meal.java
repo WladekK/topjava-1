5d383b2a1c11392e3ec91f4c5b7b6daebfe3e591
WladekK <kalinichenkovladimirv@gmail.com>
2018-11-24 22:36:20 +0200

    HW 4

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Meal extends AbstractBaseEntity {
+
     private LocalDateTime dateTime;
 
     private String description;
